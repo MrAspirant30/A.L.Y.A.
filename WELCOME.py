aecb822ca74039654170a24cd83be76add0b70a3
JyotiprasadBorgohain <jyotiprasad.borgohain24@gmail.com>
2026-06-29 19:39:54 +0530

    Level 1

--- WELCOME.py
+++ WELCOME.py
@@ -1,4 +1,8 @@
 print(" =============================================\n ")
 print("               VERSION - 0.1                  \n ")
 print(" =============================================\n ")
-print("WELCOME SIR.")
+print("WELCOME SIR.\n")
+
+print("I'm A.L.Y.A., What's your name?\n")
+name= input(" ")
+print(f"Welcome, {name}. It's nice to meet you!\n")
