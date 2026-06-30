7bd3d261f49be2aaf919123c05a2395119cc04ab
JyotiprasadBorgohain <jyotiprasad.borgohain24@gmail.com>
2026-06-30 21:29:03 +0530

    level 2

--- WAKE UP.py
+++ WAKE UP.py
@@ -1,8 +1,16 @@
 print(" =============================================\n ")
 print("               VERSION - 0.1                  \n ")
 print(" =============================================\n ")
-print("WELCOME SIR.\n")
+print("Hello, SIR/MA'AM.\n")
 
-print("I'm A.L.Y.A., What's your name?\n")
-name= input(" ")
+name= input("I'm A.L.Y.A., What's your name?\n")
 print(f"Welcome, {name}. It's nice to meet you!\n")
+
+print("I'm gonna ask you some questions to build your profile in my database\n")
+
+age= int(input("What is your age?\n"))
+fav_prog_lang= str(input())
+adress= str(input("How should I adress you?"))
+
+print("Your profile dasboard:\n")
+print(f"{adress},\nYou are {age} year old and your favourite programming language is {fav_prog_lang}\nCorrect me if I'm wrong!")
