78d8f435cd1412b150d2fe0b0d8b57741fe09046
JyotiprasadBorgohain <jyotiprasad.borgohain24@gmail.com>
2026-06-30 21:45:03 +0530

    Level 2

--- USER_PROFILE.py
+++ USER_PROFILE.py
@@ -9,8 +9,8 @@ print(f"Welcome, {name}. It's nice to meet you!\n")
 print("I'm gonna ask you some questions to build your profile in my database\n")
 
 age= int(input("What is your age?\n"))
-fav_prog_lang= str(input())
-adress= str(input("How should I adress you?"))
+fav_prog_lang= str(input("What is your favourite programming language?\n"))
+adress= str(input("How should I adress you?\n"))
 
 print("Your profile dasboard:\n")
 print(f"{adress},\nYou are {age} year old and your favourite programming language is {fav_prog_lang}\nCorrect me if I'm wrong!")
