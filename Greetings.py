08a0c07d4872f35d2aed29c085cb31a0a334b2e1
JyotiprasadBorgohain <jyotiprasad.borgohain24@gmail.com>
2026-07-01 21:59:03 +0530

    Level 3

--- Greetings.py
+++ Greetings.py
@@ -1,10 +1,10 @@
 time = str(input("Enter the Morning/Afternoon/Evening:\t"))
 
-if time == "Morning":
-    print("Good morning,")
-elif time == "Afternoon":
-    print("Good afternoon,")
-elif time == "Evening":
-    print("Good evening,")
+if time == "Morning" | "morning":
+    print("Good morning, sir")
+elif time == "Afternoon" | "afternoon":
+    print("Good afternoon, sir")
+elif time == "Evening" | "evening":
+    print("Good evening, sir")
 else:
     print("Wrong input!")
