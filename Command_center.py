ac01b2be3a45b9cef600dfaa946961357de62207
JyotiprasadBorgohain <jyotiprasad.borgohain24@gmail.com>
2026-07-02 20:58:17 +0530

    Level 4

---
+++ Command_center.py
@@ -0,0 +1,13 @@
+choice = input("Enter your choice(A/B/C):\nA. Research on any topic\nB. Simulation of any hypothetical situation\nC. Discussion of any topic\n")
+
+match choice:
+    case "A" | "a":
+        print("Let's research on the topic\n")
+    case "B" | "b":
+        print("Tell me about the situation to be simulated\n")
+    case "C" | "c":
+        print("Let's discuss on the topic\n")
+    case "D" | "d":
+        print("Tell me what you wanna do today\n")
+    case _:
+        print("")
