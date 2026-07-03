819bf5dd1cd49476a2e33f37bddb29fe0715c605
JyotiprasadBorgohain <jyotiprasad.borgohain24@gmail.com>
2026-07-03 22:11:52 +0530

    Level 5

--- Command_center.py
+++ Command_center.py
@@ -1,16 +1,12 @@
-choice = input("Enter your choice(A/B/C):\nA. Research on any topic\nB. Simulation of any hypothetical situation\nC. Discussion of any topic\n")
-
 while 1:
-    if choice == "A" or "a" or "B" or "C" or "c":
+     choice = input("Enter your choice(A/B/C):\nA. Research on any topic\nB. Simulation of any hypothetical situation\nC. Discussion of any topic\n")
      match choice:
         case "A" | "a":
             print("Let's research on the topic\n")
-            break
         case "B" | "b":
             print("Tell me about the situation to be simulated\n")
-            break
         case "C" | "c":
-            print("Let's discuss on the topic\n")
-            break  
-    else:
-        print("Tell me what you wanna do today or should I suggest something?")
+            print("Let's discuss on the topic\n") 
+        case _:
+         print("Tell me what you wanna do today or should I suggest something?")
+         break
