35732e6597d4cc6d1195330ee5b73c1160779106
JyotiprasadBorgohain <jyotiprasad.borgohain24@gmail.com>
2026-07-05 22:32:21 +0530

    Level 6

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,32 @@
-choice = input("choice: ")
+print("=" * 50)
+print("Calculator Mode".center(50))
+print("=" * 50)
+
+while 1 :
+ print("[A] Addition\n[B] Substraction\n[C] Multiplication\n[D] Division\n")
+ choice = input("choice: ")
+ match choice :
+  case "A":
+   a = float(input("\nEnter 1st number: "))
+   b = float(input("\nEnter 2nd number: "))
+   print(f"\n{a} + {b} = {a+b: .2f}")
+  case "B":
+   a = float(input("\nEnter 1st number: "))
+   b = float(input("\nEnter 2nd number: "))
+   print(f"\n{a} - {b} = {a-b: .2f}\n")
+   print(f"{b} - {a} = {b-a: .2f}")
+  case "C":
+   a = float(input("\nEnter 1st number: "))
+   b = float(input("\nEnter 2nd number: "))
+   print(f"\n{a} x {b} = {a*b: .2f}\n")
+  case "D":
+   a = float(input("\nEnter 1st number: "))
+   b = float(input("\nEnter 2nd number: "))
+   print(f"\n{a} % {b} = {a/b: .2f}\n")
+   print(f"{b} % {a} = {b/a: .2f}")
+  case _:
+   print("\nIncorrect choice!")
+   
+
+
+   
