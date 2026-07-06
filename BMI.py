3a21230d6dc2a90d3964e35c9220cc1966f55833
JyotiprasadBorgohain <jyotiprasad.borgohain24@gmail.com>
2026-07-06 22:46:18 +0530

    Level 7

--- BMI.py
+++ BMI.py
@@ -5,4 +5,4 @@ print("="*50)
 height = float(input("Height(in cm):\n"))
 weight = float(input("Weight(in Kg):\n"))
 
-print(f"BMI = {height/pow(weight, 2)}")
+print(f"BMI = {weight*10000/(pow(height, 2)): .2f} Kg/m^2")
