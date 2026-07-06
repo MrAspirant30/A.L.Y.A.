d88355d43050e4ac4f34c8b7a60415837af65d87
JyotiprasadBorgohain <jyotiprasad.borgohain24@gmail.com>
2026-07-06 22:19:55 +0530

    Level 7

--- BMI.py
+++ BMI.py
@@ -2,6 +2,7 @@ print("="*50)
 print("BMI Mode".center(50))
 print("="*50)
 
-height = float(input("Height:\n"))
-weight = float(input("Weight:\n"))
+height = float(input("Height(in cm):\n"))
+weight = float(input("Weight(in Kg):\n"))
 
+print(f"BMI = {height/pow(weight, 2)}")
